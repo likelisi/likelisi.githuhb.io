099d02438f2600432bbb06127e9ed7b74db8b63c
likelisi <114063273+likelisi@users.noreply.github.com>
2025-02-18 22:24:56 +0800

    Update get_perks.tsx

--- components/get_perks.tsx
+++ components/get_perks.tsx
@@ -12,9 +12,9 @@ export async function getManifest() {
 
   const data = await response.json();
   const destinySandboxPerkDefinition =
-    data.Response.jsonWorldComponentContentPaths."zh-chs".DestinySandboxPerkDefinition;
+    data.Response.jsonWorldComponentContentPaths.en.DestinySandboxPerkDefinition;
 
-  const url_v2 = "https://www.bungie.net" + destinySandboxPerkDefinition;
+  const url_v2 = "https://www.bungie.net" + "/common/destiny2_content/json/zh-chs/DestinySandboxPerkDefinition-7cf462e9-6581-43d2-bcdf-0456377f5873.json";
   const response_v2 = await fetch(url_v2);
   const data_v2 = await response_v2.json();
   
